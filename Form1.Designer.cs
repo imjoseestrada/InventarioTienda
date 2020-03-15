58bddb6433550ab61f3d1c7f2c09fe58f8f8eba5
Jose Estrada <j.estrada49@icloud.com>
2020-03-15 16:26:28 -0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace InventarioTienda
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbCategoria = new System.Windows.Forms.ComboBox();
+            this.dgvProductos = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(137, 36);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Categoría";
+            // 
+            // cbCategoria
+            // 
+            this.cbCategoria.FormattingEnabled = true;
+            this.cbCategoria.Location = new System.Drawing.Point(245, 33);
+            this.cbCategoria.Margin = new System.Windows.Forms.Padding(6);
+            this.cbCategoria.Name = "cbCategoria";
+            this.cbCategoria.Size = new System.Drawing.Size(291, 33);
+            this.cbCategoria.TabIndex = 1;
+            this.cbCategoria.SelectionChangeCommitted += new System.EventHandler(this.cbCategoria_SelectionChangeCommitted);
+            // 
+            // dgvProductos
+            // 
+            this.dgvProductos.BackgroundColor = System.Drawing.Color.White;
+            this.dgvProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProductos.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.dgvProductos.Location = new System.Drawing.Point(14, 92);
+            this.dgvProductos.Margin = new System.Windows.Forms.Padding(6);
+            this.dgvProductos.Name = "dgvProductos";
+            this.dgvProductos.Size = new System.Drawing.Size(644, 178);
+            this.dgvProductos.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(51)))));
+            this.ClientSize = new System.Drawing.Size(672, 282);
+            this.Controls.Add(this.dgvProductos);
+            this.Controls.Add(this.cbCategoria);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("DM Sans", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Productos por categoría";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbCategoria;
+        private System.Windows.Forms.DataGridView dgvProductos;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InventarioTienda
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (conexionTiendaDataContext dbTienda = new conexionTiendaDataContext())
+            {
+                cbCategoria.DataSource = dbTienda.Categorias.ToList();
+                cbCategoria.DisplayMember = "NombreCategoria";
+                cbCategoria.ValueMember = "IdCategoria";
+                Categoria objCategoria = cbCategoria.SelectedItem as Categoria;
+                if (objCategoria != null)
+                {
+                    dgvProductos.DataSource = dbTienda.ObtenerProductosPorIdCategoria(objCategoria.IdCategoria);
+                }
+            }
+        }
+
+        private void cbCategoria_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            Categoria objCategoria = cbCategoria.SelectedItem as Categoria;
+            if (objCategoria != null)
+            {
+                using (conexionTiendaDataContext dbTienda = new conexionTiendaDataContext())
+                {
+                    dgvProductos.DataSource = dbTienda.ObtenerProductosPorIdCategoria(objCategoria.IdCategoria);
+                }
+            }
+        }
+    }
+}
